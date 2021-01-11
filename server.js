da4888d1bc63ce34d8875b0d72b0523f5c262829
hmlu36 <hmlu36@gmail.com>
2021-01-11 11:18:37 +0800

    'deploy'

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-const lineNotify = require('./src/lineNotify');
+const { bottender, LineNotify } = require('bottender');
 
+const app = bottender({
+    dev: process.env.NODE_ENV !== 'production',
+});
 
 const port = Number(process.env.PORT) || 5000;
-const clientId = process.env.LINE_NOTIFY_CLIENT_ID;
-const clientSecret = process.env.LINE_NOTIFY_CLIENT_SECRET;
+
+// the request handler of the bottender app
+const handle = app.getRequestHandler();
+
+const lineNotify = new LineNotify({
+    clientId: process.env.LINE_NOTIFY_CLIENT_ID,
+    clientSecret: process.env.LINE_NOTIFY_CLIENT_SECRET,
+    redirectUri: `${process.env.ROOT_PATH}/notify/redirect`,
+});
+
 
 app.prepare().then(() => {
     const server = express();
@@ -20,8 +31,9 @@ app.prepare().then(() => {
 
     server.get('/notify/redirect', async function (req, res) {
         const code = req.query.code;
-        const token = await lineNotify.getToken(code, redirectUri, clientId, clientSecret);
+        const token = await lineNotify.getToken(code);
         await lineNotify.sendNotify(token, '預報');
+        res.send('Subscribe successfully. Please close this page.');
     });
 
     // route for webhook request
@@ -31,5 +43,6 @@ app.prepare().then(() => {
 
     server.listen(port, err => {
         if (err) throw err;
+        console.log(`> Ready on http://localhost:${port}`);
     });
 });
--- src/index.js
+++ src/index.js
@@ -7,16 +7,7 @@ const AQI = require('./actions/AQI');
 const Rain = require('./actions/Rain');
 const Forecast = require('./actions/Forecast');
 const UVI = require('./actions/UVI');
-const cron = require('node-cron');
 const lineNotify = require('./lineNotify');
-
-
-cron.schedule('0 50 9 * * *', (context) => {
-  const url = lineNotify.getAuthLink('notify');
-  context.sendText(url);
-});
-
-
 
 module.exports = function App(context) {
   return router([
@@ -24,7 +15,7 @@ module.exports = function App(context) {
     text(/^AQI.*$/i, AQI),
     text(/^雨量.*$/i, Rain),
     text(/^衛星.*$/i, Satellite),
-    text(/^溫度.*$/i, Temperature),
+    text(/^(溫度|氣溫).*$/i, Temperature),
     text(/^(UVI|紫外線).*$/i, UVI),
     text(/^([天氣]?)預報.*$/i, Forecast),
     text(/^.*$/i, () => {
