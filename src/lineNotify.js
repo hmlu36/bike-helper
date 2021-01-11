557c27e9a6d75b0bd5ef9b0f5cb1b315cc5c442c
hmlu36 <hmlu36@gmail.com>
2021-01-11 16:50:19 +0800

    'deploy'

--- src/lineNotify.js
+++
@@ -1,7 +0,0 @@
-const { LineNotify } = require('bottender');
-
-module.exports = new LineNotify({
-  clientId: process.env.LINE_NOTIFY_CLIENT_ID,
-  clientSecret: process.env.LINE_NOTIFY_CLIENT_SECRET,
-  redirectUri: `${process.env.ROOT_PATH}/notify/redirect`,
-});
