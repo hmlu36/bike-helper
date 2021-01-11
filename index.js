8c4b3c93ba0292fc878a335c0a115ae2e450afd0
hmlu36 <hmlu36@gmail.com>
2021-01-11 14:39:59 +0800

    'deploy'

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-/*
 module.exports = require('./src');
-*/
 
-module.exports = async function App(context) {
-    const url = `${process.env.ROOT_PATH}/notify/new`;
-    await context.sendText(url);
-  };
--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-const path = require('path');
-
 const bodyParser = require('body-parser');
 const express = require('express');
-const ejs = require('ejs');
-const { bottender, LineNotify } = require('bottender');
+const { bottender } = require('bottender');
 
 const app = bottender({
-  dev: process.env.NODE_ENV !== 'production',
+    dev: process.env.NODE_ENV !== 'production',
 });
 
 const port = Number(process.env.PORT) || 5000;
@@ -14,37 +11,22 @@ const port = Number(process.env.PORT) || 5000;
 // the request handler of the bottender app
 const handle = app.getRequestHandler();
 
-const lineNotify = new LineNotify({
-  clientId: process.env.LINE_NOTIFY_CLIENT_ID,
-  clientSecret: process.env.LINE_NOTIFY_CLIENT_SECRET,
-});
-
 app.prepare().then(() => {
-  const server = express();
+    const server = express();
 
-  server.use(
-    bodyParser.json({
-      verify: (req, _, buf) => {
+    const verify = (req, _, buf) => {
         req.rawBody = buf.toString();
-      },
-    })
-  );
+    };
+    server.use(bodyParser.json({ verify }));
+    server.use(bodyParser.urlencoded({ extended: false, verify }));
 
+    // route for webhook request
+    server.all('*', (req, res) => {
+        return handle(req, res);
+    });
 
-  server.get('/notify/redirect', async function(req, res) {
-    const code = req.query.code;
-    const token = await lineNotify.getToken(code);
-    await lineNotify.sendNotify(token, '預報');
-    res.send('Subscribe successfully. Please close this page.');
-  });
-
-  // route for webhook request
-  server.all('*', (req, res) => {
-    return handle(req, res);
-  });
-
-  server.listen(port, err => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
-  });
+    server.listen(port, err => {
+        if (err) throw err;
+        console.log(`> Ready on http://localhost:${port}`);
+    });
 });
